b0248373e0e49a1531b3f6dd7007339b3d613fa9
matthew <mwoolnerdeakos@gmail.com>
2016-07-24 02:58:11 -0400

    Much better modularization, functional gamebot

--- WikiGame.py
+++ WikiGame.py
@@ -1,41 +1,94 @@
 import requests
 import threading
 import queue
-import WikiTree
+from WikiHTMLParser import WikiParser
 
-#Constants
-PROTOCOL = "http://"
-LANGUAGE = "en"
-BASE_URL = "wikipedia.org"
-   
+class WikiNode:
+    def __init__(self,title,url,childUrls=[],parent=None):
+        self.url = url
+        self.title = title
+        self.parent = parent
+        self.childUrls = childUrls
+        self.children = []
+        self.layer = self._getLayer()
 
+    def printBranch(self):
+        if self.parent is None:
+            print(self.title,'')
+        else:
+            self.parent.printBranch()
+            print(" ---> " + self.title,'')
 
+    def _getLayer(self):
+        if self.parent is None:
+            return 1;
+        else:
+            return 1 + self.parent._getLayer()
+
+    def addChild(self,childNode):
+        self.children.append(childNode)
+
+    def __eq__(self,otherNode):
+        if self.url == otherNode.url or self.title == otherNode.title:
+            return True
+        else:
+            return False
 def Main():
     
 
+    checkedNodes = []
+    #Get user urls. 
+    #TODO: Switch to argparse for a command line interface
     firstUrl = input("Enter starting Url: ")
     goalUrl = input("Enter finishing Url: ")
     try:
-        r = requests.get(firstUrl)
+        rootNode = UrlToWikiNode(firstUrl)
+        goalNode = UrlToWikiNode(goalUrl)
     except:
-        print("Error: That URL could not be reached")
+        print("Error: There was a problem with one of your URLs")
         exit()
-
-    root = WikiTree.WikiNode(firstUrl)
-    tree = WikiTree.WikiTree(root)
-
-
-    while q.empty == False and found == False:
-        for url in urlChildren:
-           
-           if checkUrl(url) == True then:
+    
+    #ensure that 
+    checkedNodes.append(rootNode)
+    #Start a queue for nodes
+    q = queue.Queue()
+    q.put(rootNode)
+    found = False
+    resultNode = None
+    while not found and q.qsize != 0:
+        currentNode = q.get()
+        print("Testing Layer" + str(currentNode.layer))
+        #For each url, create a node. Check to see if it is the goal page
+        #If it is, stop. If not, add it to the queue and add the url to checked
+        #urls
+        for wikiUrl in currentNode.childUrls:
+            n = UrlToWikiNode(wikiUrl,currentNode)
+            print(n.title + ": " + n.url)
+            print(goalNode.title + ": " + goalNode.url)
+            if n == goalNode:
                 found = True
+                resultNode = n
+                break
             else:
+                if n not in checkedNodes:
+                    q.put(n)
+                    currentNode.addChild(n)
+
+    if not resultNode is None:
+        print("Shortest Number of Clicks: " + str(resultNode.layer))
+        resultNode.printBranch()
+    else:
+        print('No path found.')
 
 
-def createProperURL(url):
-    return PROTOCOL + LANGUAGE + "." = BASE_URL + url
-
+#Creates a WikiNode from a Wikipedia URL
+#TODO: Validate the given url
+def UrlToWikiNode(url,parent=None):
+    print("Converting: " + url + " to node.")
+    r = requests.get(url)
+    p = WikiParser()
+    p.feed(r.text)
+    return WikiNode(p.getTitle(),p.getUrl(),p.getLinkedWikis(True,True),parent)
 
 if __name__ == "__main__":
      Main()
--- WikiHTMLParser.py
+++ WikiHTMLParser.py
@@ -1,15 +1,21 @@
 from html.parser import HTMLParser
 
 class WikiParser(HTMLParser):
-    
+    #Constructor
     def __init__(self):
         super(WikiParser,self).__init__() 
         self._wikititle = ""
         self._linkedwikis = []
+        self._url = ""
         self._handlingName = False
+    
+    #Start Tag Handler
     def handle_starttag(self,tag,attrs):
         self._lasttag = tag
+       #if this is a link
         if tag == "a":
+            #If it's a link, make sure it's a wiki link and pass it to linked
+            #wikis
 
             for name,value in attrs:
                 if name == "href":
@@ -18,6 +24,13 @@ class WikiParser(HTMLParser):
                             self._linkedwikis.append(value)
                     except:
                         pass
+        #If it's a link, search for the canonical tag as that has the current
+        #URL
+        elif tag == "link":
+
+            for name,value in attrs:
+                if value == "canonical":
+                    self._url = attrs[1][1]
 
         if tag == "h1":
             
@@ -28,11 +41,34 @@ class WikiParser(HTMLParser):
     def handle_data(self,data):
         if self._handlingName:
             self._wikititle = data
-            self._handlingName = False            
+            self._handlingName = False     
+    
 
     def getTitle(self):
         return self._wikititle
 
-    def getLinkedWikis(self):
-        return self._linkedwikis
+    def getUrl(self):
+        return self._url
+
+    def getLinkedWikis(self,unique=False,fullUrls=False):
+       
+        BASE_URL = "http://en.wikipedia.org"
+        wikiUrls = self._linkedwikis
+        resultWikis = []
+        if fullUrls:
+            for wiki in wikiUrls:
+                resultWikis.append(BASE_URL + wiki)
+
+            wikiUrls = resultWikis
+        
+        if unique == False:
+            return wikiUrls
+        else:
+            #Return a version of linked wikis with duplicates removed
+            duplicateFree = []
+            for wiki in wikiUrls:
+                if wiki not in duplicateFree:
+                    duplicateFree.append(wiki)
+
+            return duplicateFree
 
